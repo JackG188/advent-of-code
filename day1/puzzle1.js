be63e4d659cc9f157c8ffe555fe588f63159f903
Jack Graham <jackmgraham@hotmail.co.uk>
2018-12-03 15:54:01 +0000

    Day 1 Part 2

--- day1/puzzle1.js
+++ day1/puzzle1.js
@@ -1,11 +1,26 @@
 const fs = require('fs');
 let result = 0;
+let results = [];
 fs.readFile('input.txt',(err, fd) => {
     if (err) {
         throw err;
     }
-    fd.toString().split('\r\n').map(item => parseInt(item)).forEach(freqChange => {
-        result = result + freqChange;
+    const inputs = fd.toString().split('\r\n').map(item => parseInt(item));
+    let inputSum = 0;
+    inputs.forEach(input => {
+        inputSum += input
     });
-    console.log(result);
+    const findFirstDouble = (inputArray) => {
+        while(true) {
+            for (let freqChange of inputArray) {
+                result += freqChange;
+                if (results.includes(result)) {
+                    return result;
+                }
+                results.push(result);
+            };
+        }
+    }
+
+    console.log('Part 1: ' + inputSum, 'Part 2: ' + findFirstDouble(inputs));
 });
